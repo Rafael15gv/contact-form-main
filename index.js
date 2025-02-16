c967c5f7a4c5208d7077e9cd43b3d097fab5d7d7
Rafael15gv <143367099+Rafael15gv@users.noreply.github.com.>
2025-02-16 17:29:59 -0500

    Se creo una rama nueva donde se mejoro html y se trato aplicaco el estilo BEM al CSS

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let boton = document.getElementsByTagName('button')[0];
 // let body = document.getElementsByTagName('body')[0];
 
 
 
 
 //CODIGO PARA DARLE COLOR AL BOTON CUANDO SE DA CLIC A CONSETN**********************************
+let boton = document.getElementsByTagName('button')[0];
 document.getElementById("consent").addEventListener("change", function() {
   if (this.checked) {
     boton.style.backgroundColor = "var(--green-medium-color)";
@@ -21,11 +21,12 @@ document.getElementById("consent").addEventListener("change", function() {
 
 
 
-  // CODIGO PARA VALIDAR INFORMACION***************************************
+//   // CODIGO PARA VALIDAR INFORMACION***************************************
 
-document.getElementById("miFormulario").addEventListener("submit", function(event) {
+document.getElementById("formulario").addEventListener("submit", function(event) {
   event.preventDefault(); // Evita que la página se recargue
-
+  
+  let check = document.getElementsByTagName("input")[5].checked;
   let nombre = document.getElementById("fname").value;
   let apellido = document.getElementById("lname").value;
   let email = document.getElementById("email").value;
@@ -35,34 +36,40 @@ document.getElementById("miFormulario").addEventListener("submit", function(even
   
   let error = document.getElementById("prueba");
   let error2 = document.getElementById("prueba2");
-
-  if (nombre.trim() === "") {
-    // alert("El campo no puede esta vacio");
-    event.preventDefault();
-    error.classList.remove("error");
-  } if (apellido.trim() === "") {
-    // alert("El campo no puede esta vacio");
-    event.preventDefault();
-    error2.classList.remove("error");
-  } if (apellido.trim() === "") {
-    // alert("El campo no puede esta vacio");
-    event.preventDefault();
-    error2.classList.remove("error");
-  }if (apellido.trim() === "") {
-    // alert("El campo no puede esta vacio");
-    event.preventDefault();
-    error2.classList.remove("error");
-  }  
   
-  
-  
-  else {
-    alert("Formulario enviado con éxito!");
-    error.classList.add("error");
-    error2.classList.add("error");
-  }
+  if (check == true) {
 
+  
 
+            event.preventDefault();
+            if (nombre.trim() === "") {
+              // alert("El campo no puede esta vacio");
+              error.style.display = "block";
+              // error.classList.remove("error");
+            } if (apellido.trim() === "") {
+              // alert("El campo no puede esta vacio");
+              error2.style.display = "block";
+            } if (apellido.trim() === "") {
+              // alert("El campo no puede esta vacio");
+              error.style.display = "block";
+            }if (apellido.trim() === "") {
+              error.style.display = "block";
+            }  
+            
+            
+            
+            else {
+              alert("Formulario enviado con éxito!");
+              error.style.display = "none";
+              error2.style.display = "none";
+            }
+          }
+          
 });
 
+
+// if (check == true) {
+
+  
+// }
 
